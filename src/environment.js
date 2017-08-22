83b92414b216a419f35b8065c1b9300f2457a876
Darius.Sobczak <darius.sobczak@db-n.com>
2017-08-22 11:51:18 +0200

    update version add environment setting on runtime, ignore tmp

--- src/environment.js
+++ src/environment.js
@@ -3,14 +3,30 @@
  */
 const argv = require('minimist')(process.argv.slice(2));
 
+const PRODUCTION = 'production';
+const STAGING = 'staging';
+const DEVELOPMENT = 'development';
+const ENVIRONMENTS = [PRODUCTION, STAGING, DEVELOPMENT];
+
 function hasFlag(flags) {
   return process.argv.filter(a => flags.includes(a)).length > 0;
 }
 // todo not static
 argv.isWatch = hasFlag(['--watch', '-w']);
 argv.isHelp = hasFlag(['--help', '-h']);
-argv.isDebug = true;
-argv.environment = process.env.hasOwnProperty('ENVIRONMENT') ? process.env.ENVIRONMENT : 'production';
+argv.isDebug = false;
+argv.environment = PRODUCTION;
+argv.environments = ENVIRONMENTS;
 argv.hasFlag = hasFlag;
+
+// set env by user environment
+if (process.env.hasOwnProperty('ENVIRONMENT')) {
+  argv.environment = process.env.ENVIRONMENT;
+}
+
+// set env by flag
+if (argv.env && ENVIRONMENTS.includes(argv.env)) {
+  argv.environment = argv.env;
+}
 
 module.exports = argv;
